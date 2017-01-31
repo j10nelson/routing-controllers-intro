50d22453a47f900c1b432646e76e532d92edab9a
Justin <justintenser@me.com>
2017-01-31 14:48:47 -0500

    halfway

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def welcome
+  @header = "This is the welcome page"
+  end
+
+  def about
+    render :about
+  end
+
+  def contest
+  end
+  
+  def kitten
+  requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+end
+end
