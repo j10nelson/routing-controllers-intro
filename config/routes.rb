1fc2d7390af03fa446e6021accf224086800b56e
Justin <justintenser@me.com>
2017-01-31 14:59:27 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get '/welcome' => 'pages#welcome'
 
-  get '/' => 'pages#welcome'
+
+root 'pages#welcome'
+get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
-  get '/kitten/:size' => 'pages#kitten'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
   get '/secrets/:magic_word' => 'pages#secrets'
 
 
