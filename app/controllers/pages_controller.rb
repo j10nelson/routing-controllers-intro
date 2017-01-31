b8aa92d2fdfa8d8165709bfc89fd9eb4bdd73aab
Justin <justintenser@me.com>
2017-01-31 14:55:26 -0500

    almsot done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
   def welcome
   @header = "This is the welcome page"
   end
@@ -8,10 +10,22 @@ class PagesController < ApplicationController
   end
 
   def contest
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
   end
-  
+
   def kitten
+end
+
+def kittens
+end
+
+def set_kitten_url
   requested_size = params[:size]
   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+end
+
+def secrets
+  flash[:alert] = "Sorry, you're not authorized to see that page!"
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
   get '/kitten/:size' => 'pages#kitten'
+  get '/secrets/:magic_word' => 'pages#secrets'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
